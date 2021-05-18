ba155a6b666d43a374b154f0fb6232d1dc0efe5c
ngld <ngld@tproxy.de>
2021-05-18 18:10:04 +0200

    chore: add proper license

--- vscode-ext/src/extension.ts
+++ vscode-ext/src/extension.ts
@@ -16,18 +16,25 @@ function startClient(context: ExtensionContext): void {
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: 'file', language: 'fso-table' }],
 		outputChannel: output,
+		traceOutputChannel: output,
 	};
 
 	client = new LanguageClient('fsoTables', 'FSO Tables LSP', serverOptions, clientOptions, true);
 	client.trace = Trace.Verbose;
-	client.start();
+	client.clientOptions.errorHandler = client.createDefaultErrorHandler(4);
+
+	context.subscriptions.push(client.start());
+	output.appendLine('Launched LSP');
 }
 
 export function activate(context: ExtensionContext) {
 	output = window.createOutputChannel('FSO Tables');
+	context.subscriptions.push(output);
+
 	startClient(context);
 
 	context.subscriptions.push(commands.registerCommand('fso-tables.restart', () => {
+		output.appendLine('Restarting LSP');
 		client.stop();
 		setTimeout(() => {
 			startClient(context);
